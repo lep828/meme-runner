01bafeb6c4ff02680098fde94f0250bafa218ab6
lep828 <lep828@gmail.com>
2016-03-24 19:12:19 +0000

    have a loop detecting collision with one object

--- js/app.js
+++ js/app.js
@@ -14,31 +14,65 @@ $(function(){
     })
   })
 
-  var $death = $("#death");
-  var $player = $("#player");
-  console.log($death.position().left, $player.position().left)
+
+
+  setInterval(function(){
+    checkCollision();
+  }, 20)
 
 
 
-
-  if($death.position().left === $player.position().left) {
-    console.log('now')
-    checkCollision($player, $death);
-  }
+  // if($death.position().left === $player.position().left) {
+  //   console.log('now')
+  //   checkCollision($player, $death);
+  // }
 })
 
-function checkCollision(player, death){
-  var $scoreDisplay = $("#score");
-  var score = 0;
 
-  var dheight = 320 - death.position().top
-  var pheight = 320 - player.position().top
+function checkCollision() {
 
-  if(pheight < dheight){
-    console.log("collision");
-    alive = false;
-  } else {
-    score++;
-    $scoreDisplay.html(score);
+  console.log("running")
+
+  var $death = $("#death");
+  var $player = $("#player");
+
+  var player = {
+    x: $player.offset().left,
+    y: $player.offset().top,
+    width: 40, 
+    height: 40
+  }
+
+  var death = {
+    x: $death.offset().left,
+    y: $death.offset().top,
+    width: 40,
+    height: 80
+  }
+
+  if (player.x < death.x + death.width &&
+   player.x + player.width > death.x &&
+   player.y < death.y + death.height &&
+   player.height + player.y > death.y) {
+      // collision detected!
+    console.log('boom')
   }
 }
+
+
+
+// function checkCollision(player, death){
+//   var $scoreDisplay = $("#score");
+//   var score = 0;
+
+//   var dheight = 320 - death.position().top
+//   var pheight = 320 - player.position().top
+
+//   if(pheight < dheight){
+//     console.log("collision");
+//     alive = false;
+//   } else {
+//     score++;
+//     $scoreDisplay.html(score);
+//   }
+// }
