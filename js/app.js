3fc1d5f85d7df1d07ca7ca6e53bf4d749bed6303
lep828 <lep828@gmail.com>
2016-03-25 16:14:06 +0000

    trying to fix flying above playArea issue

--- js/app.js
+++ js/app.js
@@ -11,20 +11,21 @@ $(function(){
   setInterval(function(){
     $death = $(".death");
     score++;
-    $scoreDisplay.html(score);
-
+    $scoreDisplay.html("Current Score = " +score);
+    // checkHeight();
     if(checkCollision($death) === true){
       if(score > highScore){
         highScore = score;
-        $highScore.html("Highscore = "+highScore)
+        $highScore.html("Highscore = " +highScore)
       }
       score = 0;
+      $scoreDisplay.html("Current Score = " +score);
     }
   }, 100);
 
   $(".death").animate({
-    "right": "850px"
-  }, 3000);
+    "right": "800px"
+  }, 2500);
 
   setInterval(function(){
       // var enemy = new createEnemy($death.offset().left, $death.offset().top, 40, 80)
@@ -32,6 +33,15 @@ $(function(){
       createEnemy($death.offset().left, $death.offset.top, 40, 90)      
       $(".death").slice(1).remove();
     }, 3000)
+  function createEnemy(x, y, width, height){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height; 
+    $(".death").animate({
+      "right": "850px"
+    }, 2500);
+  }
 })
 
 function bindEvents(){
@@ -40,30 +50,33 @@ function bindEvents(){
 
 function playerMove(){
   $("body").on("keyup", function () {
-    $("#player").stop().animate({
-      bottom: "+=150px"
-    }, 400, function () {
-      $("#player").animate({
-        bottom: "0"
-      }, 600);
-    })
-  })
-}
 
-function createEnemy(x, y, width, height){
-  this.x = x;
-  this.y = y;
-  this.width = width;
-  this.height = height; 
-  $(".death").animate({
-    "right": "850px"
-  }, 2500);
+    if($("#player").position().top <= 0){
+      console.log("boom")
+      // $("#player").off("keyup").stop().clearQueue();
+      // $("#player").unbind("keyup").clearQueue().stop();
+      // $("#player").css("top", "1", "position", "fixed")
+
+      
+      return false 
+
+    } else {
+
+      $("#player").stop().animate({
+        bottom: "+=150px"
+      }, 400, function () {
+        $("#player").animate({
+          bottom: "0"
+        }, 600);
+      })
+    }
+  })
 }
 
 function checkCollision(death) {
   console.log("running")
-  
   var $player = $("#player");
+
   var playerSize = {
     x: $player.offset().left,
     y: $player.offset().top,
