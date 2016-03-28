00ea1443704f97b3559921cb19e59b2d3750dde7
lep828 <lep828@gmail.com>
2016-03-28 09:51:28 +0100

    cleaned up some code

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ var bluck = bluck || {}
 
 
 $(function(){
-
   bluck.bindEvents();
   $("audio")[1].play();
 })
@@ -32,13 +31,11 @@ bluck.refreshers = function(){
   var highScore = 0;
   var score = 0;
   bluck.$enemy = $(".enemy");
-  console.log(bluck.$highScore)
 
   setInterval(function(){
     bluck.$enemy = $(".enemy");
     score++;
     bluck.$scoreDisplay.html("Current Score = " + score);
-
     bluck.removeEnemy();
 
     if(bluck.checkCollision() === true){
@@ -57,6 +54,7 @@ bluck.refreshers = function(){
 
 bluck.randomSpawn = function(){
   var randomNum = Math.random()
+
   if(randomNum < 0.4){
     return "$enemy1";
   } else if(randomNum < 0.6){
@@ -71,8 +69,8 @@ bluck.randomSpawn = function(){
 }
 
 bluck.addEnemy = function(){
-  bluck.enemySpeed = 1500;
   var randomEnemy = bluck.randomSpawn();
+  bluck.enemySpeed = 1500;
 
   switch(randomEnemy){
     case "$enemy1":
@@ -108,10 +106,10 @@ bluck.addEnemy = function(){
   }
 }
 
-bluck.playAudio = function(){
-  bluck.audio = $("audio")[0]
-  bluck.audio.load();
-  bluck.audio.play();
+bluck.playJumpAudio = function(){
+  var audio = $("audio")[0]
+  audio.load();
+  audio.play();
 }
 
 bluck.playerMove = function(){
@@ -125,15 +123,11 @@ bluck.playerMove = function(){
     if (e.keyCode === 0 || e.keyCode === 32) {
       e.preventDefault()
       $("#space").css("background-image", "url('http://i.imgur.com/EwBCKl7.png')")
-      bluck.playAudio();
+      bluck.playJumpAudio();
       if($("#player").position().top <= 0){
-        console.log("boom")
-        // fix this ????!?!?!?!?!?!?
         $("#player").stop().animate({bottom: "0"}, 600)
         return false 
-
       } else {
-
         $("#player").stop().animate({
           bottom: "+=100px"
         }, 400, function () {
@@ -155,7 +149,6 @@ bluck.checkCollision = function(){
     width: 40, 
     height: 40
   }
-
   var enemySize = {
     x: $(".enemy").offset().left,
     y: $(".enemy").offset().top,
@@ -170,171 +163,5 @@ bluck.checkCollision = function(){
     return true 
   }
 }
-
-
-
-// function bindEvents(){
-//   playerMove();
-//   $("#start").one("click", function(){
-//     $("#start").fadeOut(1500);
-//     startGame();
-//   });
-// }
-
-// function startGame(){
-//   refreshers();
-//   setInterval(addEnemy, 1500);
-// }
-
-// function removeEnemy(){
-//   if($(".enemy").length > 1){
-//     $(".enemy").slice(1).remove();
-//   }
-// }
-
-// function refreshers(){
-//   var $scoreDisplay = $("#score");
-//   var $highScore = $("#highScore");
-//   var highScore = 0;
-//   var score = 0;
-//   var $enemy = $(".enemy");
-
-//   setInterval(function(){
-//     $enemy = $(".enemy");
-//     score++;
-//     $scoreDisplay.html("Current Score = " + score);
-
-//     removeEnemy();
-
-//     if(checkCollision() === true){
-//       if(score > highScore){
-//         highScore = score;
-//         $highScore.html("Highscore = " + highScore);
-//       }
-//       score = 0;
-//       $scoreDisplay.html("Current Score = 0");
-//       enemySpeed = 850;
-//       levelStart = false;
-//       $("audio")[2].play();
-//       $(".enemy").stop();
-//     }
-//   }, 20);
-// }
-
-// function randomSpawn(){
-//   var randomNum = Math.random()
-//   if(randomNum < 0.4){
-//     return "$enemy1";
-//   } else if(randomNum < 0.6){
-//     return "$enemy2";
-//   } else if(randomNum < 0.75){
-//     return "$enemy3";
-//   } else if(randomNum < 0.9){
-//     return "$enemy4"
-//   } else {
-//     return "$enemy5"
-//   }
-// }
-
-// function addEnemy(){
-//   var enemySpeed = 1500;
-//   var randomEnemy = randomSpawn();
-
-//   switch(randomEnemy){
-//     case "$enemy1":
-//     $("#playArea").prepend("<div class=enemy id=enemy1></div>")
-//     $("#enemy1").animate({
-//       "right": "760px"
-//     }, enemySpeed);
-//     break;
-//     case "$enemy2":
-//     $("#playArea").prepend("<div class=enemy id=enemy2></div>")
-//     $("#enemy2").animate({
-//       "right": "760px"
-//     }, enemySpeed);
-//     break;
-//     case "$enemy3":
-//     $("#playArea").prepend("<div class=enemy id=enemy3></div>")
-//     $("#enemy3").animate({
-//       "right": "760px"
-//     }, enemySpeed);
-//     break;
-//     case "$enemy4":
-//     $("#playArea").prepend("<div class=enemy id=enemy4></div>")
-//     $("#enemy4").animate({
-//       "right": "760px"
-//     }, enemySpeed);
-//     break;
-//     case "$enemy5":
-//     $("#playArea").prepend("<div class=enemy id=enemy5></div>")
-//     $("#enemy5").animate({
-//       "right": "760px"
-//     }, enemySpeed);
-//     break;
-//   }
-// }
-
-// function playAudio(){
-//   var audio = $("audio")[0]
-//   audio.load();
-//   audio.play();
-// }
-
-// function playerMove(){
-//   $(window).keydown(function (e) {
-//     if (e.keyCode === 0 || e.keyCode === 32) {
-//       $("#space").css("background-image", "url('http://i.imgur.com/3aYa5Y1.png')")
-//     }
-//   })
-
-//   $(window).keyup(function (e) {
-//     if (e.keyCode === 0 || e.keyCode === 32) {
-//       e.preventDefault()
-//       $("#space").css("background-image", "url('http://i.imgur.com/EwBCKl7.png')")
-//       playAudio();
-//       if($("#player").position().top <= 0){
-//         console.log("boom")
-//         // fix this ????!?!?!?!?!?!?
-//         $("#player").stop().animate({bottom: "0"}, 600)
-//         return false 
-
-//       } else {
-
-//         $("#player").stop().animate({
-//           bottom: "+=100px"
-//         }, 400, function () {
-//           $("#player").animate({
-//             bottom: "0"
-//           }, 600);
-//         })
-//       }
-//     }
-//   })
-// }
-
-// function checkCollision() {
-//   var $player = $("#player");
-
-//   var playerSize = {
-//     x: $player.offset().left,
-//     y: $player.offset().top,
-//     width: 40, 
-//     height: 40
-//   }
-
-//   var enemySize = {
-//     x: $(".enemy").offset().left,
-//     y: $(".enemy").offset().top,
-//     width: 40,
-//     height: 120
-//   }
-
-//   if (playerSize.x < enemySize.x + enemySize.width &&
-//    playerSize.x + playerSize.width > enemySize.x &&
-//    playerSize.y < enemySize.y + enemySize.height &&
-//    playerSize.height + playerSize.y > enemySize.y) {
-//     return true 
-//   }
-// }
 
 
